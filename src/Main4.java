7f5dc7d5841137b80d913cb52b646fd4079c3388
beemovier <erick129ferreira@gmail.com>
2025-03-25 05:33:19 -0300

    passado tudo para o vscode e rodando no terminal

--- src/Main4.java
+++ src/Main4.java
@@ -1,10 +1,11 @@
 public class Main4 {
     public static void main(String [] args){
-        String pokemon = "caradepau";
-
+        String pokemon = "caradepau"; // Esta variável pode ser utilizada no código
 
         System.out.print("O pokemon mais forte é o: ");
+        System.out.println(pokemon);  // Agora a variável pokemon será impressa
+
         String texto = "Seupai";
-        System.out.println("Chama o seu " + texto);
+        System.out.println("Chama o seu " + texto);  // Este comando continua correto
     }
 }
