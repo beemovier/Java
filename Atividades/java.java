440e51f0b3ac83784e7f4b5c749e5c13dbbd7725
BeeVe <erick129ferreira@gmail.com>
2025-04-04 20:01:04 -0300

    Create java.java

---
+++ Atividades/java.java
@@ -0,0 +1,14 @@
+//Comparando valores de entrada
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Integer inteiro = 3;
+        
+        if( inteiro.getClass().getName()== "java.lang.Integer"){
+       System.out.println(inteiro.getClass().getName());
+       System.out.println("Deu super certo");
+        }
+       
+        
+    }
+}
