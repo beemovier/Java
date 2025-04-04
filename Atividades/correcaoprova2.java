c2bed06de412819e2f0dfcc9f452020bc04c06a4
BeeVe <erick129ferreira@gmail.com>
2025-04-04 19:57:37 -0300

    Update correcaoprova2.java

--- Atividades/correcaoprova2.java
+++ Atividades/correcaoprova2.java
@@ -1,16 +1,26 @@
 public class Main {
     public static void main(String[] args) {
         int a, b, c;  // Declaração das variáveis
-        a = 10;        // Atribui valor a a
+        a = 15;        // Atribui valor a a
         c = 0;         // Inicializa c
 
         // Operações com as variáveis
-        a = a + 5;     // a = 10 + 5 = 15
-        b = 5;         // (b assume implicitamente o valor de 20 pelo enunciado)
-        c = a * b;     // c = 15 * 5 = 75
-        a = a / 3;     // a = 15 / 3 = 5
-
+        a = a * 3;     // a = 15 * 3 = 45
+        b = 10;        // b = 10
+        c = a + b;     // c = 45 + 10 = 55
+        a = a - 6;     // a = 45 - 6 = 39
+        c = c * 2;     // c = 55 * 2 = 110
+        a = a / 5;     // a = 39 / 5 = 7 (divisão inteira)
+        
         // Exibe os valores de a, b e c
         System.out.println(a + " " + b + " " + c);  // Exibe: a, b e c
     }
 }
+/**
+ * Alternativas possíveis:
+ * 1. 7 10 110
+ * 2. 7 10 100
+ * 3. 7 20 110
+ * 4. 10 10 110
+ * 5. 15 10 110
+ */
