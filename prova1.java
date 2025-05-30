082ec834dfb48c1d75304a0a56647259a1493632
BeeVe <erick129ferreira@gmail.com>
2025-05-30 13:01:20 -0300

    Create prova1.java
    usando while pra estimar

---
+++ prova1.java
@@ -0,0 +1,16 @@
+public class prova1 {
+    public static void main(String[] args){
+
+        float rubeola = 100000f, catapora = 200000f;
+                int tempo = 1;
+
+        while(rubeola<catapora){
+            rubeola = (rubeola*1.1f);
+            catapora = (catapora*1.06f);
+            tempo++;
+        }
+        System.out.println(" o aumeunto é " + tempo*90 );
+
+    }
+}
+//esqueci de adicionar o f. era melhor com double
