32be216c20eceb29e568c5d6c4d63fa0291ee2c3
Erick <erick129ferreira@gmail.com>
2025-06-09 01:33:42 -0300

    testes de compilador

---
+++ Main.java
@@ -0,0 +1,11 @@
+// Main.java
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Salve, mundo cruel!");
+    }
+
+    /** Método inútil para testar javadoc */
+    public static int soma(int a, int b) {
+        return a + b;
+    }
+}
